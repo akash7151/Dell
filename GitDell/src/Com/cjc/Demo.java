da5f661ea4859e4e78b925e9271add0ffce0fbd3
Akash <Akash@DESKTOP-KAL629A>
2022-06-15 14:45:09 +0530

    abc

--- GitDell/src/Com/cjc/Demo.java
+++ GitDell/src/Com/cjc/Demo.java
@@ -7,6 +7,12 @@ public class Demo {
 	public void m2(){
 		System.out.println("m2");
 	}
+	public void m3(){
+		System.out.println("m3");
+	}
+	public void m4(){
+		System.out.println("m4");
+	}
 	public static void main(String[] args) {
 System.out.println("Main method");
 	}
